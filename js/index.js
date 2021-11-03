4c9b14cad35348acc57a741c3aedaa5e3fe285f8
joanribes <91140408+joanribes@users.noreply.github.com>
2021-11-03 12:20:41 +0100

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,3 @@
+for(let i = 0; i < 10; i++) console.log('WAIT!');
+console.log('if someone told you to copy paste anything here there\'s a 11/10 chance you\'re being scammed');
+console.log('Credit: https://discord.com/');
