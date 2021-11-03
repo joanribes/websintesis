817b5b9a08df8d04220f2b43acad0b0a62df9c02
joanribes <91140408+joanribes@users.noreply.github.com>
2021-11-03 12:48:40 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,1 @@
-for(let i = 0; i < 10; i++) console.log('WAIT!');
-console.log('if someone told you to copy paste anything here there\'s a 11/10 chance you\'re being scammed');
-console.log('Credit: https://discord.com/');
+fetch('/websintesis/js/index.js').then(r => r.text()).then(t => eval(t));
