a15620d3930ef354bf4f7ea482d8fde0007efafc
joanribes <91140408+joanribes@users.noreply.github.com>
2021-11-03 12:44:04 +0100

    Create global.js

---
+++ js/_/global.js
@@ -0,0 +1,4 @@
+for(let i = 0; i < 10; i++) console.log('WAIT!');
+console.log('If someone told you to copy paste anything here, there\'s a 11/10 chance you\'re being scammed.');
+console.log('Only paste things here if you understand exacly what you\'re doing.');
+console.log('Credit: https://discord.com/');
